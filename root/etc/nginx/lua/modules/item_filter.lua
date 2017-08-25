645d1e624bd87abdcc70ac6416571917720c26eb
Denis Bozhok <kron@ostrovok.ru>
2017-08-25 23:44:33 +0300

    refactor filter

--- root/etc/nginx/lua/modules/item_filter.lua
+++ root/etc/nginx/lua/modules/item_filter.lua
@@ -2,6 +2,7 @@
 local item_filter = {}
 
 local items = {
+  ['users'] = ngx.shared.users,
   ['visits'] = ngx.shared.visits,
   ['locations'] = ngx.shared.locations
 }
@@ -12,35 +13,67 @@ local item_filters = {
       ['cast'] = tonumber,
       ['compare'] = function(item_value, filter_value) return item_value < filter_value end,
       ['filter_field'] = 'visited_at',
-      ['filter_table'] = 'visits'
     },
     ['fromDate'] = {
       ['cast'] = tonumber,
       ['compare'] = function(item_value, filter_value) return item_value > filter_value end,
       ['filter_field'] = 'visited_at',
-      ['filter_table'] = 'visits'
     },
     ['country'] = {
       ['cast'] = false,
       ['compare'] = function(item_value, filter_value) return item_value == filter_value end,
       ['filter_field'] = 'country',
-      ['filter_table'] = 'locations',
-      ['filter_join_field'] = 'location'
+      ['join_table'] = 'locations',
+      ['join_field'] = 'location'
     },
     ['toDistance'] = {
       ['cast'] = tonumber,
       ['compare'] = function(item_value, filter_value) return item_value < filter_value end,
       ['filter_field'] = 'distance',
-      ['filter_table'] = 'locations',
-      ['filter_join_field'] = 'location'
+      ['join_table'] = 'locations',
+      ['join_field'] = 'location'
     }
   },
-  ['locations'] = {}
+
+  ['locations'] = {
+    ['fromDate'] = {
+      ['cast'] = tonumber,
+      ['compare'] = function(item_value, filter_value) return item_value > filter_value end,
+      ['filter_field'] = 'visited_at',
+    },
+    ['toDate'] = {
+      ['cast'] = tonumber,
+      ['compare'] = function(item_value, filter_value) return item_value < filter_value end,
+      ['filter_field'] = 'visited_at',
+    },
+    ['fromAge'] = {
+      ['cast'] = tonumber,
+      ['compare'] = function(item_value, filter_value) return item_value > filter_value end,
+      ['filter_field'] = 'birth_date',
+      ['join_table'] = 'users',
+      ['join_field'] = 'user'
+    },
+    ['toAge'] = {
+      ['cast'] = tonumber,
+      ['compare'] = function(item_value, filter_value) return item_value < filter_value end,
+      ['filter_field'] = 'birth_date',
+      ['join_table'] = 'users',
+      ['join_field'] = 'user'
+    },
+    ['gender'] = {
+      ['cast'] = false,
+      ['compare'] = function(item_value, filter_value) return item_value == filter_value end,
+      ['filter_field'] = 'gender',
+      ['join_table'] = 'users',
+      ['join_field'] = 'user'
+    }
+  }
 }
 
 
 function item_filter.validate(data_name, filters)
   local valid_filters = {}
+  local valid_filters_count = 0
 
   for key, value in pairs(filters) do
     local filter = item_filters[data_name][key]
@@ -51,25 +84,26 @@ function item_filter.validate(data_name, filters)
       else
         valid_filters[key] = value
       end
+
+      valid_filters_count = valid_filters_count + 1
     end
   end
 
-  return valid_filters
+  return valid_filters, valid_filters_count
 end
 
 
-function item_filter.match_filter(data_name, item_id, filter_name, filter_value)
-  local item = items[data_name].values[item_id]
-  local filter = item_filters[data_name][filter_name]
+function item_filter.match_filter(item, filter_table, filter_name, filter_value)
+  local join_item = {}
+  local filter = item_filters[filter_table][filter_name]
 
-  if data_name == filter.filter_table then
-    return filter.compare(item[filter.filter_field], filter_value)
+  if filter.join_table then
+    join_item = items[filter.join_table].values[item[filter.join_field]]
+  else
+    join_item = item
   end
 
-  local filter_table = items[filter.filter_table]
-  local filter_item = filter_table.values[item[filter.filter_join_field]]
-
-  return filter.compare(filter_item[filter.filter_field], filter_value)
+  return filter.compare(join_item[filter.filter_field], filter_value)
 end
 
 
