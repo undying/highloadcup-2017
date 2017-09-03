8f7c52d27a2d050caab2b9be29fbe22dc21321d9
Denis Bozhok <kron@ostrovok.ru>
2017-09-03 15:13:31 +0300

    pin redis to last cpu

--- root/sbin/run_redis.sh
+++ root/sbin/run_redis.sh
@@ -4,7 +4,7 @@ CPU_COUNT=$(grep -c processor /proc/cpuinfo)
 cpu_n=$[CPU_COUNT-1]
 
 
-taskset -c 0 redis-server /etc/redis.conf
+taskset -c ${cpu_n} redis-server /etc/redis.conf
 
 ##for i in locations users visits;do
 ##  taskset -c ${cpu_n} redis-server /etc/redis_${i}.conf
