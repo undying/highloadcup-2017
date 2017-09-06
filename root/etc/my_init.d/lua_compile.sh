4a3c19342b8875ab2ba58b4501343fd8d6ba3dc7
Denis Bozhok <kron@ostrovok.ru>
2017-09-06 17:07:36 +0300

    compile all lua modules

---
+++ root/etc/my_init.d/lua_compile.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+CPU_COUNT=$(grep -c processor /proc/cpuinfo)
+
+time \
+  find \
+  /etc/nginx \
+  /usr/local/share/lua/5.1 \
+  -type f -iname '*.lua' \
+  |sed -e 's,\.lua,,g' \
+  |xargs -P${CPU_COUNT} -L1 -I{} luajit-2.1.0-beta3 -O3 -b {}.lua {}.ljbc
+
+
