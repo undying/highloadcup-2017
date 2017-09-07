88524aa0f39f74d3386da7175fb70ec97c2a33ae
Denis Bozhok <kron@ostrovok.ru>
2017-09-07 13:39:21 +0300

    use master

--- root/sbin/run_redis.sh
+++ root/sbin/run_redis.sh
@@ -1,15 +1,6 @@
 #! /bin/bash -x
 
-CPU_COUNT=$(grep -c processor /proc/cpuinfo)
-cpu_n=$[CPU_COUNT-1]
-
 #taskset 8 redis-server /etc/redis.conf
-redis-server /etc/redis.conf
-
-##for i in locations users visits;do
-##  taskset -c ${cpu_n} redis-server /etc/redis_${i}.conf
-##
-##  cpu_n=$[cpu_n - 1]
-##  [ ${cpu_n} -ge 0 ] || cpu_n=$[CPU_COUNT - 1]
-##done
+redis-server /etc/redis-master.conf
+redis-server /etc/redis-slave.conf
 
