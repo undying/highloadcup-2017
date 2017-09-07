ad6a3cf830d3bbb0b79a2a21250095200ace0790
Denis Bozhok <kron@ostrovok.ru>
2017-09-07 13:34:59 +0300

    +master/slave redis

--- root/etc/nginx/lua/init.lua
+++ root/etc/nginx/lua/init.lua
@@ -19,7 +19,7 @@ local redis = require('resty_redis')
 ngx.shared.storage_redis.options = {}
 ngx.shared.storage_redis.options.keepalive_idle = 600000
 ngx.shared.storage_redis.options.keepalive_pool = 8192
-ngx.shared.storage_redis.options.connect = { '127.0.0.1', 6379 }
+ngx.shared.storage_redis.options.connections = { {'unix:/run/redis-master.sock'}, {'unix:/run/redis-slave.sock'} }
 --
 
 -- loading options.txt data
