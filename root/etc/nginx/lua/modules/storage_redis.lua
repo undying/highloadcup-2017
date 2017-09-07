af71864772e58fee35ae6931a8da407c3a702de9
Denis Bozhok <kron@ostrovok.ru>
2017-09-07 13:37:40 +0300

    connection choser

--- root/etc/nginx/lua/modules/storage_redis.lua
+++ root/etc/nginx/lua/modules/storage_redis.lua
@@ -4,22 +4,46 @@ local redis = require('resty_redis')
 
 
 function storage_redis.connect()
-  local r = redis:new()
-  local ok, err = r:connect(unpack(ngx.shared.storage_redis.options.connect))
+  local connections = {}
 
-  if not ok then
-    ngx.log(ngx.STDERR, 'redis connection failed')
-    return
+  for index, conn in pairs(ngx.shared.storage_redis.options.connections) do
+    local r = redis:new()
+    local ok, err = r:connect(unpack(conn))
+
+    if not ok then
+      ngx.log(ngx.STDERR, 'redis connection failed')
+      return
+    end
+
+    table.insert(connections, r)
   end
 
-  return r
+  return connections
 end
 
 
-function storage_redis.set_keepalive(connection)
-  connection:set_keepalive(
-  ngx.shared.storage_redis.options.keepalive_idle,
-  ngx.shared.storage_redis.options.keepalive_pool)
+function storage_redis.chose(mode, connections)
+  if mode == 'w' then
+    if not connections[1] then return nil end
+    return connections[1]
+  end
+
+  local to_chose_cnt = 0
+  for index, connection in pairs(connections) do
+    to_chose_cnt = to_chose_cnt + 1
+  end
+
+  return connections[(ngx.time() % to_chose_cnt) + 1]
+end
+
+
+function storage_redis.set_keepalive(connections)
+  for _, connection in pairs(connections) do
+    connection:set_keepalive(
+      ngx.shared.storage_redis.options.keepalive_idle,
+      ngx.shared.storage_redis.options.keepalive_pool
+    )
+  end
 end
 
 
