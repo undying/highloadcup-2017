db336d83ee7caa57199f52611cabfcaa43da6eb8
Denis Bozhok <kron@ostrovok.ru>
2017-09-08 20:27:59 +0300

    just naming

--- root/etc/nginx/lua/init.lua
+++ root/etc/nginx/lua/init.lua
@@ -18,7 +18,7 @@ local redis = require('resty_redis')
 -- redis connection options
 ngx.shared.storage_redis.options = {}
 ngx.shared.storage_redis.options.keepalive_idle = 600000
-ngx.shared.storage_redis.options.keepalive_pool = 8192
+ngx.shared.storage_redis.options.keepalive_pool_size = 8192
 ngx.shared.storage_redis.options.connections = { {'unix:/run/redis-master.sock'} }
 --
 
