26c178533ce5e805d10e4cb594bbe1f001479a99
Denis Bozhok <kron@ostrovok.ru>
2017-09-08 20:00:26 +0300

    tune monitor

--- root/sbin/socket_monitor.sh
+++ root/sbin/socket_monitor.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+sockets=0
+
 while :;do
-  printf "redis sockets in use: %d\n" $(ss -x|grep -c redis)
-  sleep 10
+  sockets_last=$(ss -x|grep -c redis)
+  [ ${sockets_last} -eq ${sockets} ] || printf "redis sockets in use: %d\n" ${sockets_last}
+
+  sockets=${sockets_last}
+  sleep 1
 done
 
