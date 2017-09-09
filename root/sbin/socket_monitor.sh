fb5950f74692d5b3e9750a64242eaf49661cf717
Denis Bozhok <kron@ostrovok.ru>
2017-09-09 23:40:09 +0300

    pin redis to cpu

--- root/sbin/socket_monitor.sh
+++ root/sbin/socket_monitor.sh
@@ -7,7 +7,7 @@ while :;do
   unix_sockets_last=$(ss -x|grep -c redis)
   tcp_sockets_last=$(ss -et dst :16379|wc -l)
 
-  [ ${unix_sockets_last} -eq ${unix_sockets} ] || printf "redis sockets in use: %d\n" ${unix_sockets_last}
+  [ ${unix_sockets_last} -eq ${unix_sockets} ] || printf "redis unix sockets in use: %d\n" ${unix_sockets_last}
   [ ${tcp_sockets_last} -eq ${tcp_sockets} ] || printf "redis tcp sockets in use: %d\n" ${tcp_sockets_last}
 
   unix_sockets=${unix_sockets_last}
